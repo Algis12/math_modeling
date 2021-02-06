2c57a3612baa0012ffc0d2cf95fc835fc328df3c
Algis12 <71917437+Algis12@users.noreply.github.com>
2021-02-06 12:46:07 +0200

    Add files via upload

---
+++ lec_4_create_fanc.py
@@ -0,0 +1,10 @@
+def mult_func(a):
+  x = 3*a
+  return x
+tmp = mult_func(4)
+print(tmp)
+print(mult_func(10))
+print(mult_func('Good'))
+def print_func(a):
+  print(f'Я печатаю: {a}')
+print_func('hello')
---
+++ lec_4_documenting.py
@@ -0,0 +1,8 @@
+def crutoi_chuvak(a=1, b=1, c=1):
+  '''Это функция
+  '''
+  a='pofig'
+  b='pofig'
+  print('Крутой чувак')
+crutoi_chuvak()
+help(crutoi_chuvak)
